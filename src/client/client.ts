00ee40be8bd67839d755ec9f4d7caa4dfd35625d
tananh <bu.tanh96@gmail.com>
2023-12-09 13:26:38 +0700

    christmas update

--- src/client/client.ts
+++ src/client/client.ts
@@ -11,6 +11,31 @@ function sleep(ms: number): Promise<void> {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function updateBackgroundBasedOnDate() {
+    // let shand = document.getElementsByClassName('s-hand') as HTMLCollectionOf<HTMLElement>;
+    let bg_image = document.getElementsByClassName("background-container") as HTMLCollectionOf<HTMLElement>;
+    const currentDate = new Date();
+    // const dayOfMonth = currentDate.getDate();
+    const month = currentDate.getMonth()
+    // console.log(dayOfMonth)
+    // console.log(month)
+    // Define the base path for your background images
+    const imagePath = './data/old_paper.png';
+
+    let imageName: string;
+
+    // Determine the background image based on the day of the month
+    if (month == 11) {
+        imageName = "url('./data/christmas_bg.png')";
+    } else {
+        imageName = "url('./data/old_paper.png')";
+    }
+
+    if (bg_image.length != 0){
+        bg_image[0].style.backgroundImage = imageName
+    }
+}
+
 async function displayRandomText(): Promise<void> {
     const randomTextElement = document.getElementById('randomText');
     if (randomTextElement) {
@@ -19,9 +44,10 @@ async function displayRandomText(): Promise<void> {
             randomTextElement.innerText = text.slice(0, i);
             await sleep(50); // Adjust the speed of streaming
         }
-        // randomTextElement.innerText = text;
     }
 }
 
+// update background image by date
+updateBackgroundBasedOnDate()
 // Display random text when the page loads
 displayRandomText();
